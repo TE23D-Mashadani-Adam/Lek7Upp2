a0d5f6173f2e73e6ae821bbb84975e50b286e409
Adam Mashadani <adam.mashadani@elev.ga.ntig.se>
2025-11-24 10:07:30 +0100

    Nu kan man skapa elev

--- src/App.java
+++ src/App.java
@@ -1,7 +1,39 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hej du ska skapa en skola!");
 
-        
+        System.out.println("Skapa elever, ange följande uppgifter om din första elev:");
+        boolean continueElev = true;
+        int elevNummer = 0;
+        Elev[] elever = new Elev[3];
+        while (continueElev) {
+
+            System.out.println("Ange elevnamn");
+            String elevNamn = scanner.nextLine();
+            System.out.println("Ange elev personnummer:");
+            int elevPerNum = scanner.nextInt();
+            String[] elevKurser = { "Matte1", "Fysik 2", "projg 1" };
+           
+            elever[elevNummer] = new Elev(elevNamn, elevPerNum, "Studerande", "TE23D", elevKurser);
+
+            scanner.nextLine();
+            System.out.println("Vill du lägga till ytterligare en elev? Ja eller Nej");
+            String svar = scanner.nextLine();
+            if (svar.toLowerCase().equals("ja") && elevNummer <= 4) {
+                continueElev = true;
+                elevNummer++;
+            } else if (svar.toLowerCase().equals("ja") && elevNummer > 4) {
+                continueElev = false;
+            }else{
+                continueElev = false;
+            }
+        }
+
+        for (int i = 0; i < elevNummer + 1; i++) {
+            System.out.println(elever[i].toString());
+        }
     }
 }
--- src/Elev.java
+++ src/Elev.java
@@ -1,9 +1,11 @@
+import java.util.Arrays;
+
 public class Elev extends BaseClass {
     private String klass;
     private String[] pågåendeKurser;
     
     public Elev(String namn, int perNum, String biträdesAnledning, String klass, String[] pågåendeKurser) {
-        super(namn, perNum, biträdesAnledning);
+        super(namn, perNum, "Studerande");
         this.klass = klass;
         this.pågåendeKurser = pågåendeKurser;
     }
@@ -15,6 +17,16 @@ public class Elev extends BaseClass {
     public void setPågåendeKurser(String[] pågåendeKurser) {
         this.pågåendeKurser = pågåendeKurser;
     }
+
+    @Override
+    public String toString() {
+        return "Elev [klass=" + klass + ", pågåendeKurser=" + Arrays.toString(pågåendeKurser) + ", getNamn()="
+                + getNamn() + ", getPerNum()=" + getPerNum() + ", getBiträdesAnledning()=" + getBiträdesAnledning()
+                + ", getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()=" + super.toString()
+                + "]";
+    }
+
+    
 
     
 
